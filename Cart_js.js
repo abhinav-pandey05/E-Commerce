cdecd450c5ecc25b3e14a87037b6646b002d419c
Abhinav Pandey <abhinav.05.pandey@gmail.com>
2018-04-04 17:56:09 +0530

    2nd push

--- Cart_js.js
+++ Cart_js.js
@@ -1,33 +1,30 @@
-var itemNames = document.querySelectorAll(".itemNameHeader");
 var cartInfoWrapper = document.getElementById("cartInfoWrapper");
+var buttons = document.getElementsByClassName("addButton");
 var itemQtys = document.querySelectorAll(".itemQty");
-var buttons = document.querySelectorAll("input[type=button]");
-var j;
-var counter  = 1;
 
 var itemstoAdd = [        
         {   
-            "itemID"  : 1,
+            "itemID"  : 0,
             "itemName": "Joola Carbon Blade",
             "itemQuantity": 4 
         },
         {   
-            "itemID"  : 2,
+            "itemID"  : 1,
             "itemName": "Mark V Red Rubber",
             "itemQuantity": 4 
         },
         {    
-            "itemID"  : 3,
+            "itemID"  : 2,
             "itemName": "Mark V Black Rubber", 
             "itemQuantity":4
         },
         {    
-            "itemID"  : 4,
+            "itemID"  : 3,
             "itemName": "DHS 3 star Ball", 
             "itemQuantity": 4
         },
         {    
-            "itemID"  : 5,
+            "itemID"  : 4,
             "itemName": "Stiga TT Table",
             "itemQuantity": 4 
         }
@@ -35,7 +32,17 @@ var itemstoAdd = [
 
 var Execute = (function () {
     return {
+
+        init:   function () {
+            Execute.updateQuantity();
+            for(let j = 0; j < buttons.length; j++) {
+                buttons[j].addEventListener("click", function() {
+                   Execute.fetchInventoryID(this);   
+                });
+            }
+        },
         updateQuantity: function () {
+            var itemNames = document.querySelectorAll(".itemNameHeader");
             for(let i=0;i<itemNames.length;i++) {
                 itemNames[i].innerHTML = itemstoAdd[i].itemName;
                 itemQtys[i].innerHTML = itemstoAdd[i].itemQuantity;
@@ -48,74 +55,55 @@ var Execute = (function () {
             Execute.createNodes(spanID,parentEle);
         },
         createNodes: function (spanID,parentEle) {
-            var newDiv = document.createElement("div");
-            var newInnerDiv = document.createElement("div");            
-            var newLabel = document.createElement("label");
-            var newSpan = document.createElement("span");
-            var newButton = document.createElement("button");
-            newDiv.className = "cartItemsInfo";
-            newInnerDiv.className = "cartInfo";
-            newButton.className = "removeButtons";
-            newButton.innerHTML = "Remove";
-            cartInfoWrapper.appendChild(newDiv);
-            newDiv.appendChild(newInnerDiv);
-            newInnerDiv.appendChild(newLabel);
-            newInnerDiv.appendChild(newSpan);
-            newDiv.appendChild(newButton);
-            newSpan.id = "value";
-            newLabel.innerHTML = itemstoAdd[spanID-1].itemName;
-            newSpan.innerText = itemstoAdd[spanID-1].itemQuantity;
-            newSpan.innerText = counter; 
-            // newButton.addEventListener('click',Execute.handleRemove.bind());
-            console.log(newButton);
-            newButton.addEventListener('click',Execute.handleRemove.bind(newButton));
-            if(itemstoAdd[spanID-1].itemQuantity < 1) {
-            parentEle.parentElement.style.display ="none";
+            
+            console.log(spanID , "is spanID");
+            if(!cartInfoWrapper.contains(document.getElementById('elem' +spanID))){ 
+                var newDiv = document.createElement("div");
+                var newInnerDiv = document.createElement("div");            
+                var newLabel = document.createElement("label");
+                var newSpan = document.createElement("span");
+                var newButton = document.createElement("button");
+
+                newDiv.className = "cartItemsInfo";
+                newInnerDiv.className = "cartInfo";
+                newButton.className = "removeButtons";
+                newButton.innerHTML = "Remove";
+                newSpan.setAttribute("id",'elem' + spanID);
+                
+                cartInfoWrapper.appendChild(newDiv);
+                newDiv.appendChild(newInnerDiv);
+                newInnerDiv.appendChild(newLabel);
+                newInnerDiv.appendChild(newSpan);
+                newDiv.appendChild(newButton);
+               
+                newLabel.innerHTML = itemstoAdd[spanID-1].itemName;
+                itemstoAdd[spanID-1].itemQuantity -=1 ;
+                Execute.updateQuantity();
+                newSpan.innerText = 1; 
+                console.log("new button", newButton);
+                newButton.addEventListener('click',Execute.handleRemove(spanID));
             }
-            else{
-                itemstoAdd[spanID-1].itemQuantity -= counter;
+            else {
+                let itemCount = document.getElementById('elem' +spanID).innerHTML;
+                document.getElementById('elem' +spanID).innerHTML = ++itemCount;
+                console.log("item count is ",itemCount);
+                itemstoAdd[spanID-1].itemQuantity -=1 ;
                 Execute.updateQuantity();
             }
+            
+            if(itemstoAdd[spanID-1].itemQuantity < 1) {
+                parentEle.parentElement.style.display ="none";
+            }
+            
         },
-        handleRemove: function(newButton){
-            var cartButtonsArray = cartInfoWrapper.getElementsByTagName("button");
-            // console.log("Cart info array is",cartButtonsArray);
-            // for(let c=0 ; c < cartButtonsArray.length ; c++){
-            //     if(cartButtonsArray[c].className === "removeButtons"){
-            //        var cartInfo = cartButtonsArray[c].parentElement;
-            //        var cartItemsInfo = cartInfo.parentElement;
-            //        console.log("cartButtonsArray" , cartButtonsArray[c]);
-            //        cartItemsInfo. parentNode. removeChild(cartItemsInfo);
-            console.log(newButton);
-            var cartInfo = newButton.parentElement;
-                   var cartItemsInfo = cartInfo.parentElement;
-                   console.log("cartItemsInfo is" , cartItemsInfo);
-                   cartItemsInfo. parentNode. removeChild(cartItemsInfo);
+        handleRemove: function(spanID){
+            var cartItemsInfo = this.parentElement;
+            cartItemID =  cartItemsInfo.getElementById('elem' +spanID);
+            // console.log("cartItemsInfo is" , cartItemsInfo);
             
         },
         
     }
 })();
 
-
-function init() {
-    Execute.updateQuantity();
-    for(let j=0 ; j < buttons.length ; j++) {
-        buttons[j].addEventListener("click", function() {
-           Execute.fetchInventoryID(this);
-            
-        });
-    };
-    // var outputButtons =[];
-    //  outputButtons = document.getElementsByClassName("removeButtons");
-    // for(let j=0 ; j < outputButtons.length ; j++) {
-    //     outputButtons[j].addEventListener("click", function() {
-    //         var spanElement = document.createElement("span");
-    //         outputButtons[j].appendChild(spanElement);
-    //         spanElement.innerHTML("No item is present in the cart");
-    //         //var newSpan = document.createElement("span");
-    //         // document.getElementById("cartInfoWrapper").innerHTML = "<h6> No item is present in the cart </h6>";   
-    //         });
-    //     }
-}
-init();
+Execute.init();
